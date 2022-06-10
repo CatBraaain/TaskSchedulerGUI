ac845f0251d39eab650658132dadca3d67a6f6cd
CatBraaain <chattain@gmail.com>
2022-06-10 23:08:14 +0900

    a

---
+++ エクスプローラーと同期.ps1
@@ -0,0 +1,30 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+$sch = New-Object -ComObject("Schedule.Service")
+$sch.Connect("localhost")
+
+$folders = @($sch.GetFolder("\MyTasks"))
+
+$i = 0
+while ($i -lt $folders.Length) {
+    $folders[$i].GetFolders(0) | %{
+        $folders += $_
+    }
+    $folders[$i].GetTasks(0) | %{
+        $folders[$i].DeleteTask($_.Name,$null)
+    }
+    $i += 1
+}
+for ($i=$folders.length-1; $i -ge 0; $i--){
+    $folders[$i].GetFolders(0) | % {
+        $folders[$i].DeleteFolder($(Split-Path $_.Path -Leaf),$null)
+    }
+}
+
+$base_dir = "C:\Users\PRO\Documents\TaskScheduler\MyTasks\"
+$rel_paths = Get-ChildItem $base_dir -Recurse -Filter *.xml -Name 
+
+foreach ($rel_path in $rel_paths){
+    $task_name = "MyTasks\$($rel_path.Replace('.xml',''))"
+    schtasks /create /XML $base_dir$rel_path /TN $task_name /S localhost /RU "tetu3188@yahoo.co.jp" /RP "saka5656"
+}
---
+++ タスクスケジューラと同期.ps1
@@ -0,0 +1,24 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+Remove-Item "C:\Users\PRO\Documents\TaskScheduler\MyTasks" -Recurse
+
+$sch = New-Object -ComObject("Schedule.Service")
+$sch.Connect("localhost")
+
+$folders = @($sch.GetFolder("\MyTasks"))
+$root = "C:\Users\PRO\Documents\TaskScheduler"
+
+$i = 0
+while ($i -lt $folders.Length) {
+    [System.IO.Directory]::CreateDirectory("$root$($folders[$i].Path)")
+
+    $folders[$i].GetFolders(0) | %{
+        $folders += $_
+    }
+    $folders[$i].GetTasks(0) | %{
+        $Xml = $_.Xml
+        $out_path = "$root$($folders[$i].Path)\$($_.Name).xml"
+        $Xml | Out-File $out_path
+    }
+    $i += 1
+}
